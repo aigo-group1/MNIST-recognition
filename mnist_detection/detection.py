0f7b6882024873b8b1e1c8553c573fd6d60aa529
dqhuy140598 <huylai007>
2019-04-14 17:07:22 +0700

    done6

--- mnist_detection/detection.py
+++ mnist_detection/detection.py
@@ -1,1 +1,12 @@
-print('Hello World')
+#Phần này dùng để đọc đường dẫn ảnh trong upload
+from pathlib import Path
+base_path = Path(__file__).parent
+file_path = (base_path / "../server/upload/pj 204.png").resolve()
+import matplotlib.image as mp
+image_path = str(file_path)
+image = mp.imread(image_path)
+#end
+import matplotlib.pyplot as plt
+plt.imshow(image)
+plt.show()
+
