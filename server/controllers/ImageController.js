310295d3c160ee0097970b56ba6fc6bd40dd431f
dqhuy140598 <huylai007>
2019-04-16 15:15:18 +0700

    done server

--- server/controllers/ImageController.js
+++ server/controllers/ImageController.js
@@ -1,13 +1,38 @@
 const Image = require('../models/image');
-
+const path = require('path');
+const uploadPath = path.join(__dirname,'/../');
+const {spawn} = require('child_process');
+predict = (modelPath,imagePath)=>{
+    return new Promise((get,drop)=>{
+        const pyProg = spawn('python', [modelPath,imagePath]);
+        
+        pyProg.stdout.on('data',(data)=>{
+            get(data.toString())
+        })
+        pyProg.stderr.on('err',(err)=>{
+            drop(err.toString())
+        })
+        pyProg.on('close', (code) => {
+            console.log(`child process exited with code ${code}`);
+        });
+    })
+}
 uploadImage = async (req,res)=>{
-    const name = req.file;
-    const filePath = req.file.path;
-    const image = new Image({imagePath:filePath});
-    const result = await image.save();
-    console.log(result);
-};
 
+    const filePath = req.file.path;
+
+    const pathToUpload = path.join(uploadPath,filePath);
+    
+    const image = new Image({imagePath:filePath});
+
+    const result = await image.save();
+
+    predict('../mnist_model/readImage.py',pathToUpload).then((data)=>{
+        console.log(data)
+    }).catch((err)=>{
+        console.log(err)
+    }) 
+};
 module.exports = {
     uploadImage:uploadImage
 }
--- server/mongoose.js
+++ server/mongoose.js
@@ -5,7 +5,7 @@ moongoose.connect('mongodb://127.0.0.1:27017/mnist',{
         useNewUrlParser:true,
         useCreateIndex:true
     },(err)=>{
-        if(err) console.log(err);
+        if(err) console.log(err); 
 });
 
 module.exports = moongoose;
